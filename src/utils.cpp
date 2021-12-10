da8a1d3e4e7c26c8f9713be889444e85dccab98c
Lucas Viana <oviana@outlook.com>
2021-12-10 15:55:13 -0300

    Add != operator to Position

--- src/utils.cpp
+++ src/utils.cpp
@@ -4,6 +4,10 @@ bool Position::operator==(const Position p) {
   return this->x == p.x && this->y == p.y;
 }
 
+bool Position::operator!=(const Position p) {
+  return this->x != p.x || this->y != p.y;
+}
+
 Position &Position::operator=(const Position &p) {
   x = p.x;
   y = p.y;
