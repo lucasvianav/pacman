b265f6f54e0d4f33ca57ba5ef7ce47914a61cb8f
Lucas Viana <oviana@outlook.com>
2021-12-10 16:29:45 -0300

    Refactor user input thread

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "controllers.h"
 #include "map.h"
 #include "utils.h"
+#include <chrono>
+#include <clocale>
 #include <ncurses.h>
 #include <string>
 #include <thread>
@@ -9,42 +11,77 @@
 using namespace std;
 
 int main() {
-  GameController game_ctrl;
-  Pacman pac{&game_ctrl, 14, 20};
-  Ghost inky{&game_ctrl, 12, 14};
+  setlocale(LC_ALL, "");
 
-  initscr();
+  GameController gc;
+  Pacman pac{&gc, 14, 20};
+  Ghost inky{&gc, 12, 14};
 
-  auto keypress = [&pac, &game_ctrl]() {
-    char c = 0;
+  auto keypress = [&pac, &gc]() {
+    int c;
+    Move direction;
+    int delay = INPUT_DELAY;
+    WINDOW *gamescr = gc.get_screen();
 
     while (true) {
-      c = getch();
+      c = wgetch(gamescr);
 
-      pac.update_position(static_cast<Move>(c));
-      game_ctrl.update_map('@', pac.get_previous_position(),
-                           pac.get_current_position());
+      switch (c) {
+      case KEY_UP:
+      case 'w':
+      case 'k':
+        direction = UP;
+        break;
+
+      case KEY_RIGHT:
+      case 'd':
+      case 'l':
+        direction = RIGHT;
+        break;
+
+      case KEY_DOWN:
+      case 's':
+      case 'j':
+        direction = DOWN;
+        break;
+
+      case KEY_LEFT:
+      case 'a':
+      case 'h':
+        direction = LEFT;
+        break;
+
+      default:
+        continue;
+      }
+
+      pac.move(static_cast<Move>(direction));
+
+      this_thread::sleep_for(chrono::nanoseconds(delay));
     }
   };
 
-  auto game_controller = [&]() { game_ctrl.load_map(); };
-
-  auto automove_ghosts = [&inky, &game_ctrl]() {
+  auto game_controller = [&]() {
     while (true) {
-      sleep(1);
-      inky.update_position();
-      game_ctrl.update_map('%', inky.get_previous_position(),
-                           inky.get_current_position());
+      this_thread::sleep_for(chrono::nanoseconds(MAP_REFRESH_DELAY));
+      gc.refresh();
     }
   };
 
-  thread kp = thread(keypress);
-  thread gc = thread(game_controller);
-  // thread ag = thread(automove_ghosts);
+  auto automove_ghosts = [&inky, &gc]() {
+    while (true) {
+      this_thread::sleep_for(chrono::nanoseconds(GHOST_DELAY));
+      inky.move();
+    }
+  };
 
-  kp.join();
-  gc.join();
-  // ag.join();
+  thread t_kp = thread(keypress);
+  thread t_gc = thread(game_controller);
+  thread t_ag = thread(automove_ghosts);
+
+  t_kp.join();
+  t_gc.join();
+  t_ag.join();
 
   return 0;
 }
